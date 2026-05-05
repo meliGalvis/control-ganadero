bad1db938a6e5a79d896066218594d3b257af22d
meliGalvis <96071482+meliGalvis@users.noreply.github.com>
2026-05-04 23:03:13 -0700

    Fix import statement for conectarDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { conectarDB } = require("./config/db");
+const conectarDB = require("./config/db");
 
 const app = express();
 
